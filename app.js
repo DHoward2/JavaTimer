0338889c2df343d71dc273e04d0b92e025739e96
Howard <dangelohoward@gmail.com>
2023-01-09 03:18:00 -0500

    added js for timer and listeners

---
+++ app.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("form");
+const secInput = document.querySelector("input");
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log(secInput.value);
+
+})
+
+function countDown(num){
+    
+    let timer = setInterval(function(){
+        num--;
+        
+        if(num <= 0){
+            clearInterval(timer);
+            console.log('Done')
+        }
+        else {
+            console.log(num);
+        }
+        
+
+    },1000);
+    
+
+
+  }
